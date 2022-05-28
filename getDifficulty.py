5aa6baec91002abc39cf57074e458eeca852a7a0
trcoelhoo <84328889+trcoelhoo@users.noreply.github.com>
2022-05-28 17:20:43 +0100

    Update getDifficulty.py

--- getDifficulty.py
+++ getDifficulty.py
@@ -13,6 +13,9 @@ dif_hard=0
 descartados=0
 occluded=0
 truncated=0
+noveh=0
+descartados_height=0
+descartados_trunc=0
 
 def main():
     global dif_easy
@@ -21,8 +24,11 @@ def main():
     global descartados
     global occluded
     global truncated
+    global noveh
+    global descartados_height
+    global descartados_trunc
 
-    for filesonpath in glob.glob("E:\\Datasets\\12_n\\velodyne\\*.bin"):
+    for filesonpath in glob.glob("E:\\Datasets\\13\\velodyne\\*.bin"):
     #for filesonpath in glob.glob("D:\\presil-export\\velodyne\\*.bin"):
     #for filesonpath in glob.glob("E:\\testar\\*.bin"):
         values = []
@@ -31,7 +37,7 @@ def main():
         file_split=file_name.split(".")
         print("Split: ", file_split[0])
         #output_path = "C:\\Users\\Leandro\\Desktop\\Presil\\PreSIL_Output\\bintoply\\" + file_split[0] + ".ply"
-        output_path = "E:\\Datasets\\12_n\\velodyne\\"
+        output_path = "E:\\Datasets\\13\\velodyne\\"
         txtname = output_path.replace("\\","")+".txt"
         txtname = txtname.replace("velodyne","")
         txtname=txtname[2:]
@@ -41,7 +47,7 @@ def main():
         #values_f=augmentationAttempt("D:\\presil-export\\object\\velodyne\\000013.bin",values)
         #saveBinFile(output_path,values)
         
-    t= dif_easy + dif_medium + dif_hard + descartados
+    t= dif_easy + dif_medium + dif_hard + descartados + descartados_trunc + descartados_height
     per_easy = (dif_easy/t)*100
     per_medium = (dif_medium/t)*100
     per_hard = (dif_hard/t)*100
@@ -53,8 +59,12 @@ def main():
     f.write("Medium: " + str(dif_medium) + "\n")
     f.write("Hard: " + str(dif_hard) + "\n")
     f.write("Descartados: " + str(descartados) + "\n")
+    f.write("Descartados por height <25: " + str(descartados_height) + "\n")
+    f.write("Descartados por trunc >0.5: " + str(descartados_trunc) + "\n")
+    f.write("Descartados total: " + str(descartados+descartados_height+descartados_trunc) + "\n")
     f.write("Occluded: " + str(occluded) + "\n")
     f.write("Truncated: " + str(truncated) + "\n")
+    f.write("No Vehicle on frame: " + str(noveh) + "\n")
     f.write("Total: " + str(t) + "\n")
     f.write("Percentual easy: " + str(per_easy) + "\n")
     f.write("Percentual medium: " + str(per_medium) + "\n")
@@ -77,17 +87,21 @@ def getDifficulty(file_path,name,output_file):
     global descartados
     global occluded
     global truncated
-    label_name = "E:\\Datasets\\12_n\\label_2\\"  + str(name) + ".txt"
-    dif=0
+    global noveh
+    global descartados_height
+    global descartados_trunc
+    label_name = "E:\\Datasets\\13\\label_2\\"  + str(name) + ".txt"
+
     with open(label_name) as f:
         content = f.readlines()
         #print (content)
+        flag=0
         for line in content:
             #print (line)
-
-            if (line.startswith("Car") or line.startswith("Bus") or line.startswith("Truck") or line.startswith("Motorbike") or line.startswith("Trailer")or line.startswith("Truck")or line.startswith("Misc") or line.startswith("Van")):
+            
+            if (line.startswith("Car") or line.startswith("Truck")or line.startswith("Misc") or line.startswith("Van")):
                 #print (line)
-                dif=1
+                flag=1
                 det= line.split(" ")
                 det[1]=float(det[1])
                 det[2]=float(det[2])
@@ -112,8 +126,15 @@ def getDifficulty(file_path,name,output_file):
                 elif ( det[1] <= 0.5 and ( det[2] ==2 or det[2] == 1 or det[2]==0)and height >=25):
                     #print ("hard")
                     dif_hard+=1
+                elif(height<25):
+                    descartados_height+=1 # para descartar o restante 
+                elif(det[1]>0.5):
+                    descartados_trunc+=1
                 else:
-                    descartados+=1 # para descartar o restante         
+                    descartados+=1
+        if (flag==0):
+            noveh+=1
+
         f.close()
     
     
